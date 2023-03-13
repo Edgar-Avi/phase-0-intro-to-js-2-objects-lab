e788de37960b0c8122784cac8fdadd39b279e2c9
Edgar Avila <edgaravila01@yahoo.com>
2023-03-12 18:24:13 -0700

    Phase 0 intro to js 2 objects lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+}
+function updateEmployeeWithKeyAndValue(employee, key, values) {
+    const name = "Sam"
+    delete employee.name
+    return { name: 'Sam', 
+    streetAddress: "11 Broadway"};
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name
+    return employee;
+}
